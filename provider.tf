730828ee0b385664df2522a3e5a42155ee536a22
caotritran <caotritran.14@gmail.com>
2023-01-29 06:36:33 +0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -18,8 +18,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "7d916e23-9568-4b0d-93f9-fb59905603ec"
-  tenant_id       = "4a92cb1c-6470-45cc-9de5-8111336e4d98"
-  client_id       = "b75138be-cca0-4cb6-80f3-983c1d2fc043"
-  client_secret   = "na-8Q~XuYC~JWyRCTtScbD4PzNSO53b6ktbYYaLZ"
+  subscription_id = "HERE"
+  tenant_id       = "HERE"
+  client_id       = "HERE"
+  client_secret   = "HERE"
 }
