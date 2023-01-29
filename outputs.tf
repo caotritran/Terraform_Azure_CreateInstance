81cdbd3fc47e3f78820ed35a6d4fcbef66b2ea12
caotritran <caotritran.14@gmail.com>
2023-01-29 08:59:16 +0700

    add output name

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip_address" {
   value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
+
+output "azure_vm_name" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm.name
+}
